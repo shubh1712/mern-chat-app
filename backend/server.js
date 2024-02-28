a700e11c2387a569ef91ee7561a2a069fe42a9f3
shubh1712 <shubhhhhhj17@gmail.com>
2024-02-28 11:13:44 +0530

    deployement

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js"
 import {app,server} from "./socket/socket.js"
+import path from "path"
 // const app = express()
 const PORT =  process.env.PORT || 5000
 
+const __dirname = path.resolve()
 dotenv.config()
 
 app.use(express.json()) //to parse incoming requests with JSON
@@ -19,9 +21,13 @@ app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
 
-// app.get("/",(req,res)=>{
-//     res.send(" HELLO world")
-// })
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
 
 server.listen(PORT, ()=> {
     connectToMongoDB();
